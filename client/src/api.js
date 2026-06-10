d87e31cef9ebdbc7e5d9ba75c4b77fdf4f57a00b
asiyasa <asiyasa07@gmail.com>
2026-06-10 11:37:42 +0530

    Added new feature

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000'
+  // Vite-ல் environment variable-ஐ அணுகும் முறை இது
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
 });
 
 export default API;
